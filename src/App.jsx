8f11d92c9b61b0b291bfd612f74a6620996a1279
sohail <sohailtalat455@gmail.com>
2025-09-19 08:15:10 +0300

    second commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,36 @@
+import styled from "styled-components";
+import GlobalStyle from "./styles/GlobalStyles";
+
+const H1 = styled.h1`
+  font-size: 30px;
+  font-weight: 600;
+  background-color: yellow;
+`;
+
+const StyledApp = styled.main`
+  background-color: orange;
+  padding: 20px;
+`;
+
+const Input = styled.input`
+  border-radius: 5px;
+  border: 1px solid #ddd;
+  background-color: var(--color-grey-0);
+  padding: 0.8 1.2rem;
+  box-shadow: var(--shadow-sm);
+`;
+
 function App() {
   return (
     <>
-      <p>soahil</p>
+      <GlobalStyle />
+      <StyledApp>
+        <H1>soahil</H1>
+        <Button onClick={() => alert("check in")}>check in</Button>
+        <Button onClick={() => alert("check out")}>check out</Button>
+        <Input placeholder="Number Of Guests" />
+        <Input placeholder="Number Of Cabinets" />
+      </StyledApp>
     </>
   );
 }
--- src/ui/Button.jsx
+++ src/ui/Button.jsx
@@ -47,3 +47,14 @@ const variations = {
     }
   `,
 };
+const Button = styled.button`
+  font-size: 1.4rem;
+  padding: 1.2rem 1.6rem;
+  font-weight: 400;
+  border: none;
+  border-radius: var(--border-radius-sm);
+  background-color: var(--color-brand-600);
+  color: var(--color-grey-0);
+  box-shadow: var(--shadow-sm);
+`;
+export default Button;
